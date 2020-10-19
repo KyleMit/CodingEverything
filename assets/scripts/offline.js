5d37779669e490cdbd585da78c82adbd92421294
kylemit <Kylemit@gmail.com>
2020-10-19 09:21:47 -0400

    migrate build process to create-eleventy-blog

--- assets/scripts/offline.js
+++
@@ -1,9 +0,0 @@
-function updateIndicator() {
-	document.body.classList.toggle('offline', !navigator.onLine)
-}
-
-// Update the online status icon based on connectivity
-window.addEventListener('online',  updateIndicator);
-window.addEventListener('offline', updateIndicator);
-
-updateIndicator();
